049e8742fe5d567658d57599241f6dee91b9ec56
Nathan <na.than.fjv@gmail.com>
2024-08-08 18:44:02 -0300

    feat: add brands section

--- src/pages/Home/Home.test.tsx
+++ src/pages/Home/Home.test.tsx
@@ -8,32 +8,48 @@ vi.mock("@assets/full-banner-mobile.jpg", () => ({
   default: "full-banner-mobile.jpg",
 }));
 vi.mock("@assets/full-banner.jpg", () => ({ default: "full-banner.jpg" }));
+vi.mock("@assets/brand-versace.svg", () => ({ default: "brand-versace.svg" }));
 
 describe("Home component", () => {
-  it("deve renderizar corretamente com a imagem e link", () => {
+  it("should be render image and link from banner", () => {
     render(
       <MemoryRouter>
         <Home />
       </MemoryRouter>,
     );
 
-    // Verifica se o link foi renderizado corretamente
     const linkElement = screen.getByRole("link", {
       name: /Two models, a man and a woman, dressed in stylish black and white casual outfits/i,
     });
     expect(linkElement).toBeInTheDocument();
     expect(linkElement).toHaveAttribute("href", "/");
 
-    // Verifica se a imagem desktop é renderizada
     const imgElement = screen.getByAltText(
       /Two models, a man and a woman, dressed in stylish black and white casual outfits/i,
     );
     expect(imgElement).toBeInTheDocument();
     expect(imgElement).toHaveAttribute("src", "full-banner.jpg");
 
-    // Verifica se o elemento `picture` contém o elemento `source` correto
     const sourceElement = screen.getByTestId("picture-source");
     expect(sourceElement).toHaveAttribute("srcset", "full-banner-mobile.jpg");
     expect(sourceElement).toHaveAttribute("media", "(max-width: 767px)");
+  });
+
+  it("should be render images and links from brands", () => {
+    render(
+      <MemoryRouter>
+        <Home />
+      </MemoryRouter>,
+    );
+
+    const logoElement = screen.getByRole("link", {
+      name: /Versace Logo/i,
+    });
+    expect(logoElement).toBeInTheDocument();
+    expect(logoElement).toHaveAttribute("href", "/versace");
+
+    const imgElement = screen.getByAltText(/Versace Logo/i);
+    expect(imgElement).toBeInTheDocument();
+    expect(imgElement).toHaveAttribute("src", "brand-versace.svg");
   });
 });
--- src/pages/Home/Home.view.tsx
+++ src/pages/Home/Home.view.tsx
@@ -1,22 +1,51 @@
+import calvinKleinBrand from "@assets/brand-calvin-klein.svg";
+import gucciBrand from "@assets/brand-gucci.svg";
+import pradaBrand from "@assets/brand-prada.svg";
+import versaceBrand from "@assets/brand-versace.svg";
+import zaraBrand from "@assets/brand-zara.svg";
 import fullBannerMobile from "@assets/full-banner-mobile.jpg";
 import fullBanner from "@assets/full-banner.jpg";
 import { Link } from "react-router-dom";
 
 export function Home() {
   return (
-    <Link to="/">
-      <picture>
-        <source
-          srcSet={fullBannerMobile}
-          media="(max-width: 767px)"
-          data-testid="picture-source"
-        />
-        <img
-          src={fullBanner}
-          className="h-auto w-full"
-          alt="Two models, a man and a woman, dressed in stylish black and white casual outfits, posing confidently. The image is part of a fashion advertisement with bold text that reads 'Find Clothes That Matches Your Style'. Below the text, a 'Shop Now' button is visible, followed by promotional statistics like '200+ International Brands', '2,000+ High-Quality Products', and '30,000+ Happy Customers'."
-        />
-      </picture>
-    </Link>
+    <>
+      <Link to="/">
+        <picture>
+          <source
+            srcSet={fullBannerMobile}
+            media="(max-width: 767px)"
+            data-testid="picture-source"
+          />
+          <img
+            src={fullBanner}
+            className="h-auto w-full"
+            alt="Two models, a man and a woman, dressed in stylish black and white casual outfits, posing confidently. The image is part of a fashion advertisement with bold text that reads 'Find Clothes That Matches Your Style'. Below the text, a 'Shop Now' button is visible, followed by promotional statistics like '200+ International Brands', '2,000+ High-Quality Products', and '30,000+ Happy Customers'."
+          />
+        </picture>
+      </Link>
+
+      <div className="bg-black py-10">
+        <div className="container">
+          <div className="flex items-center justify-between gap-4">
+            <Link to="/versace">
+              <img src={versaceBrand} alt="Versace logo" />
+            </Link>
+            <Link to="/zara">
+              <img src={zaraBrand} alt="Zara logo" />
+            </Link>
+            <Link to="/gucci">
+              <img src={gucciBrand} alt="Gucci logo" />
+            </Link>
+            <Link to="/prada">
+              <img src={pradaBrand} alt="Prada logo" />
+            </Link>
+            <Link to="/calvin-klein">
+              <img src={calvinKleinBrand} alt="Calvin Klein logo" />
+            </Link>
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
