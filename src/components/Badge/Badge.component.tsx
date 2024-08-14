19cbd68b53adc9239f6f54ed8269aca43688ed27
Nathan <na.than.fjv@gmail.com>
2024-08-14 15:37:22 -0300

    style: add rounded and change padding

--- src/components/Badge/Badge.component.tsx
+++ src/components/Badge/Badge.component.tsx
@@ -4,7 +4,7 @@ type BadgeProps = {
 
 export function Badge({ discountValue }: BadgeProps) {
   return (
-    <span className="flex items-center justify-center bg-red-200 px-3 py-2 text-xs text-red-600">
+    <span className="flex items-center justify-center rounded-full bg-red-200 px-2 py-1 text-xs text-red-600">
       -{discountValue}%
     </span>
   );
