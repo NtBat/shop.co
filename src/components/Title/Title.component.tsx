a8588c261c47eec431432005a2b6dfa4fa01f837
Nathan <na.than.fjv@gmail.com>
2024-08-14 14:15:11 -0300

    refact: add tailwind variants

--- src/components/Title/Title.component.tsx
+++ src/components/Title/Title.component.tsx
@@ -1,18 +1,35 @@
-type TitleProps = {
-  title: string;
-  variant?: "primary" | "secondary";
-  as?: keyof JSX.IntrinsicElements;
-};
+import { type VariantProps, tv } from "tailwind-variants";
 
-export function Title({
-  title,
-  variant = "primary",
-  as: Component = "h2",
-}: TitleProps) {
-  return (
-    <Component
-      className={`text-4xl font-bold uppercase ${variant == "primary" ? "text-black" : "text-white"}`}>
-      {title}
-    </Component>
-  );
+type TitleProps = {
+  text: string;
+  as?: keyof JSX.IntrinsicElements;
+} & VariantProps<typeof title>; // Tipo das variantes
+
+const title = tv({
+  base: "text-4xl font-bold uppercase",
+  variants: {
+    color: {
+      primary: "text-black",
+      secondary: "text-white",
+    },
+    size: {
+      sm: "text-sm",
+      md: "text-base",
+      lg: "px-4 py-3 text-lg",
+    },
+  },
+  compoundVariants: [
+    {
+      size: ["sm", "md"],
+      class: "px-3 py-1",
+    },
+  ],
+  defaultVariants: {
+    size: "md",
+    color: "primary",
+  },
+});
+
+export function Title({ text, color, size, as: Component = "h2" }: TitleProps) {
+  return <Component className={title({ size, color })}>{text}</Component>;
 }
--- src/components/Title/Title.test.tsx
+++ src/components/Title/Title.test.tsx
@@ -4,33 +4,33 @@ import { describe, expect, it } from "vitest";
 
 describe("Title component", () => {
   it("should render the title correctly", () => {
-    render(<Title title="Test Title" />);
+    render(<Title text="Test Title" />);
     const titleElement = screen.getByText("Test Title");
     expect(titleElement).toBeInTheDocument();
   });
 
   it('should apply the correct class for the "primary" variant', () => {
-    render(<Title title="Primary Title" variant="primary" />);
+    render(<Title text="Primary Title" color="primary" />);
     const titleElement = screen.getByText("Primary Title");
     expect(titleElement).toHaveClass("text-black");
     expect(titleElement.tagName).toBe("H2");
   });
 
   it('should apply the correct class for the "secondary" variant', () => {
-    render(<Title title="Secondary Title" variant="secondary" />);
+    render(<Title text="Secondary Title" color="secondary" />);
     const titleElement = screen.getByText("Secondary Title");
     expect(titleElement).toHaveClass("text-white");
     expect(titleElement.tagName).toBe("H2");
   });
 
   it('should render the title as an h1 element when the "as" prop is "h1"', () => {
-    render(<Title title="H1 Title" as="h1" />);
+    render(<Title text="H1 Title" as="h1" />);
     const titleElement = screen.getByText("H1 Title");
     expect(titleElement.tagName).toBe("H1");
   });
 
   it('should render the title as an h3 element when the "as" prop is "h3"', () => {
-    render(<Title title="H3 Title" as="h3" />);
+    render(<Title text="H3 Title" as="h3" />);
     const titleElement = screen.getByText("H3 Title");
     expect(titleElement.tagName).toBe("H3");
   });
