f48f9a2d25df2cf9fa46b1c50c245f0252edbdc4
Nathan <na.than.fjv@gmail.com>
2024-08-30 14:41:45 -0300

    style: add hover animation on footer

--- src/components/Footer/Footer.component.tsx
+++ src/components/Footer/Footer.component.tsx
@@ -48,22 +48,30 @@ export function Footer() {
             <h3 className="font-semibold uppercase">Company</h3>
             <ul className="mt-4 space-y-2">
               <li>
-                <Link to="/about" className="text-sm">
+                <Link
+                  to="/about"
+                  className="relative text-sm after:absolute after:bottom-0 after:left-0 after:h-px after:w-0 after:bg-black after:transition-all after:ease-in-out hover:after:w-full">
                   About
                 </Link>
               </li>
               <li>
-                <Link to="/features" className="text-sm">
+                <Link
+                  to="/features"
+                  className="relative text-sm after:absolute after:bottom-0 after:left-0 after:h-px after:w-0 after:bg-black after:transition-all after:ease-in-out hover:after:w-full">
                   Features
                 </Link>
               </li>
               <li>
-                <Link to="/works" className="text-sm">
+                <Link
+                  to="/works"
+                  className="relative text-sm after:absolute after:bottom-0 after:left-0 after:h-px after:w-0 after:bg-black after:transition-all after:ease-in-out hover:after:w-full">
                   Works
                 </Link>
               </li>
               <li>
-                <Link to="/career" className="text-sm">
+                <Link
+                  to="/career"
+                  className="relative text-sm after:absolute after:bottom-0 after:left-0 after:h-px after:w-0 after:bg-black after:transition-all after:ease-in-out hover:after:w-full">
                   Career
                 </Link>
               </li>
@@ -73,22 +81,30 @@ export function Footer() {
             <h3 className="font-semibold uppercase">Help</h3>
             <ul className="mt-4 space-y-2">
               <li>
-                <Link to="/customer-support" className="text-sm">
+                <Link
+                  to="/customer-support"
+                  className="relative text-sm after:absolute after:bottom-0 after:left-0 after:h-px after:w-0 after:bg-black after:transition-all after:ease-in-out hover:after:w-full">
                   Customer Support
                 </Link>
               </li>
               <li>
-                <Link to="/delivery-details" className="text-sm">
+                <Link
+                  to="/delivery-details"
+                  className="relative text-sm after:absolute after:bottom-0 after:left-0 after:h-px after:w-0 after:bg-black after:transition-all after:ease-in-out hover:after:w-full">
                   Delivery Details
                 </Link>
               </li>
               <li>
-                <Link to="/terms" className="text-sm">
+                <Link
+                  to="/terms"
+                  className="relative text-sm after:absolute after:bottom-0 after:left-0 after:h-px after:w-0 after:bg-black after:transition-all after:ease-in-out hover:after:w-full">
                   Terms & Conditions
                 </Link>
               </li>
               <li>
-                <Link to="/privacy-policy" className="text-sm">
+                <Link
+                  to="/privacy-policy"
+                  className="relative text-sm after:absolute after:bottom-0 after:left-0 after:h-px after:w-0 after:bg-black after:transition-all after:ease-in-out hover:after:w-full">
                   Privacy Policy
                 </Link>
               </li>
@@ -98,22 +114,30 @@ export function Footer() {
             <h3 className="font-semibold uppercase">Faq</h3>
             <ul className="mt-4 space-y-2">
               <li>
-                <Link to="/faq-account" className="text-sm">
+                <Link
+                  to="/faq-account"
+                  className="relative text-sm after:absolute after:bottom-0 after:left-0 after:h-px after:w-0 after:bg-black after:transition-all after:ease-in-out hover:after:w-full">
                   Account
                 </Link>
               </li>
               <li>
-                <Link to="/faq-manage-deliveries" className="text-sm">
+                <Link
+                  to="/faq-manage-deliveries"
+                  className="relative text-sm after:absolute after:bottom-0 after:left-0 after:h-px after:w-0 after:bg-black after:transition-all after:ease-in-out hover:after:w-full">
                   Manage Deliveries
                 </Link>
               </li>
               <li>
-                <Link to="/faq-orders" className="text-sm">
+                <Link
+                  to="/faq-orders"
+                  className="relative text-sm after:absolute after:bottom-0 after:left-0 after:h-px after:w-0 after:bg-black after:transition-all after:ease-in-out hover:after:w-full">
                   Orders
                 </Link>
               </li>
               <li>
-                <Link to="/faq-payments" className="text-sm">
+                <Link
+                  to="/faq-payments"
+                  className="relative text-sm after:absolute after:bottom-0 after:left-0 after:h-px after:w-0 after:bg-black after:transition-all after:ease-in-out hover:after:w-full">
                   Payments
                 </Link>
               </li>
@@ -123,22 +147,30 @@ export function Footer() {
             <h3 className="font-semibold uppercase">Resources</h3>
             <ul className="mt-4 space-y-2">
               <li>
-                <Link to="/free-ebooks" className="text-sm">
+                <Link
+                  to="/free-ebooks"
+                  className="relative text-sm after:absolute after:bottom-0 after:left-0 after:h-px after:w-0 after:bg-black after:transition-all after:ease-in-out hover:after:w-full">
                   Free eBooks
                 </Link>
               </li>
               <li>
-                <Link to="/development-tutorial" className="text-sm">
+                <Link
+                  to="/development-tutorial"
+                  className="relative text-sm after:absolute after:bottom-0 after:left-0 after:h-px after:w-0 after:bg-black after:transition-all after:ease-in-out hover:after:w-full">
                   Development Tutorial
                 </Link>
               </li>
               <li>
-                <Link to="/blog" className="text-sm">
+                <Link
+                  to="/blog"
+                  className="relative text-sm after:absolute after:bottom-0 after:left-0 after:h-px after:w-0 after:bg-black after:transition-all after:ease-in-out hover:after:w-full">
                   How to - Blog
                 </Link>
               </li>
               <li>
-                <Link to="/youtube-playlist" className="text-sm">
+                <Link
+                  to="/youtube-playlist"
+                  className="relative text-sm after:absolute after:bottom-0 after:left-0 after:h-px after:w-0 after:bg-black after:transition-all after:ease-in-out hover:after:w-full">
                   Youtube Playlist
                 </Link>
               </li>
@@ -146,7 +178,9 @@ export function Footer() {
           </div>
         </div>
         <div className="mt-5 flex flex-col items-center justify-between gap-5 border-t border-solid border-gray-200 pt-5 lg:flex-row">
-          <p className="text-sm">Shop.co © 2000-2024, All Rights Reserved</p>
+          <p className="relative text-sm after:absolute after:bottom-0 after:left-0 after:h-px after:w-0 after:bg-black after:transition-all after:ease-in-out hover:after:w-full">
+            Shop.co © 2000-2024, All Rights Reserved
+          </p>
           <div>
             <img src={paymentForm} alt="Flags credit card" loading="lazy" />
           </div>
